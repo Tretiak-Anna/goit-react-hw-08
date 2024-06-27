f8b3eb8853346e41d93f742f21dddca9c313ead0
Tretiak-Anna <tretiakann@gmail.com>
2024-06-27 16:32:05 +0300

    basic init

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "modern-normalize";
-import { Provider } from "react-redux";
-import App from "./components/App/App";
-import { store } from "./redux/store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
